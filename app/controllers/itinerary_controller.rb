649134b2f5ae7e109ce045c43c539a558f4512de
Rebecca Tay <rebeccatay92@gmail.com>
2017-09-01 11:29:37 +0800

    add controller fxns for get routes

--- app/controllers/itinerary_controller.rb
+++ app/controllers/itinerary_controller.rb
@@ -2,14 +2,30 @@ class ItineraryController < ApplicationController
 before_action -> { doorkeeper_authorize! :api }
 
   def showAll
+    user_id = current_resource_owner["id"]
+    allItineraries = Itinerary.where({user_id: user_id})
+
     render json: {
-      response: "show all itineraries"
+      response: "show all itineraries by logged in user",
+      user_id: user_id,
+      allItineraries: allItineraries
     }
   end
 
   def edit
+    user_id = current_resource_owner["id"]
+    itinerary_id = params["id"]
+    itinerary = Itinerary.find(itinerary_id)
+    activities = itinerary.activities
+    photos = activities.map{|e| Photo.where({activity_id: e.id}) }
+
     render json: {
-      response: "show and edit one itinerary"
+      response: "show one itinerary by logged in user for editing",
+      user_id: user_id,
+      itinerary_id: itinerary_id,
+      itinerary: itinerary,
+      activities: activities,
+      photos: photos
     }
   end
 
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,25 @@
 class StaticController < ApplicationController
 
   def home
+    allItineraries = Itinerary.all
     render json: {
-      response: "this is homepage"
+      response: "this is homepage",
+      allItineraries: allItineraries
     }
   end
 
   def show
+    requestedBlogId = params["id"]
+    requestedBlog = Itinerary.find(requestedBlogId)
+    activities = requestedBlog.activities
+    photos = activities.map{|e| Photo.where({activity_id: e.id}) }
+
     render json: {
-      response: "show one blog by id"
+      response: "show one blog by id",
+      requestedBlogId: params["id"],
+      requestedBlog: requestedBlog,
+      activities: activities,
+      photos: photos
     }
   end
 
