4e8429de6cf67331da611e15f04a8ec12d39e936
Rebecca Tay <rebeccatay92@gmail.com>
2017-09-02 10:25:57 +0800

    photo controller done

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -2,22 +2,44 @@ class PhotoController < ApplicationController
 before_action -> { doorkeeper_authorize! :api }
 
   def create
+    activity_id = params["activity_id"]
+    request = params["data"]
+
+    createdPhoto = Photo.create({
+      activity_id: activity_id,
+      url: request["url"]
+    })
 
     render json: {
-      response: "add a photo to activity"
+      response: "add a photo to activity",
+      activity_id: activity_id,
+      request: request,
+      createdPhoto: createdPhoto
     }
   end
 
   def update
+    photo_id = params["photo_id"]
+    request = params["data"]
+    thisPhoto = Photo.find(photo_id)
+
+    updatedPhoto = thisPhoto.update({
+      url: request["url"]
+    })
 
     render json: {
-      response: "update photo url"
+      response: "update photo url",
+      updatedPhoto: updatedPhoto
     }
   end
 
   def destroy
+    photo_id = params["photo_id"]
+    deletedPhoto = Photo.delete(photo_id)
+
     render json: {
-      response: "remove this photo"
+      response: "remove this photo",
+      deletedPhoto: deletedPhoto
     }
   end
 
